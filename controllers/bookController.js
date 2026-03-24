fa14c3f73135f7d7ba71121c16df9ac8838269f0
Bhavika Daga <48dagabhavika@gmail.com>
2026-03-24 12:09:30 +0530

    feat: add book controller with all routes logic

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -10,7 +10,7 @@ const getAllBooks = (req, res) => {
 const getBooksByGenre = (req, res) => {
     try{
         const bookGenre = books.filter(g=> g.genre === req.params.genre);
-        if(bookGenre === 0) return res.status(404).json({message: "Book not Found"});
+        if(bookGenre.length === 0) return res.status(404).json({message: "Book not Found"});
         res.status(200).json(bookGenre);
     }catch(err){
         res.status(500).json({message: err.message});
@@ -20,7 +20,7 @@ const getBooksByGenre = (req, res) => {
 const getBooksByCupboard = (req, res) => {
     try{
         const bookCupboard = books.filter(c => c.cupboard === parseInt(req.params.cupboard));
-        if(bookCupboard.length === 0) return res.status(404).json({message: "No cupboard found"});
+        if(bookCupboard.length === 0) return res.status(404).json({message: "No cupboard umber found"});
         return res.status(200).json(bookCupboard);
     }catch(err){
         res.status(500).json({message: err.message});
@@ -28,23 +28,53 @@ const getBooksByCupboard = (req, res) => {
 };
 
 const getBooksByShelf = (req, res) => {
-
-};
-
-const getBooksByCupboardAndShelf = (req, res) => {
-
+    try{
+        const bookShelf = books.filter(s => s.shelf === parseInt(req.params.shelf));
+        if(bookShelf.length === 0) return res.status(404).json({message: "No shelf number found"});
+        return res.status(200).json(bookShelf);
+    }catch(err){
+        res.status(500).json({message: err.message});
+    }
 };
 
 const getBookById = (req, res) => {
+    try{
+        const bookId = books.find(s => s.id === parseInt(req.params.id));
+        if(!bookId) return res.status(404).json({message: "No book id found"});
+        return res.status(200).json(bookId);
+    }catch(err){
+        res.status(500).json({message: err.message});
+    }
+};
 
+const getBooksByCupboardAndShelf = (req, res) => {
+  try {
+    const bookByCS = books.filter(b => b.cupboard === parseInt(req.params.cupboard) && b.shelf === parseInt(req.params.shelf));
+    if (bookByCS.length === 0) return res.status(404).json({ message: "No books found" });
+    return res.status(200).json(bookByCS);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
 const getIssuedBooks = (req, res) => {
-
+    try{
+        const bookIssued = books.filter(s => s.isIssued === true);
+        if(bookIssued.length === 0) return res.status(404).json({message: "No issued book found"});
+        return res.status(200).json(bookIssued);
+    }catch(err){
+        res.status(500).json({message: err.message});
+    }
 };
 
 const getAvailableBooks = (req, res) => {
-
+  try {
+    const availableBooks = books.filter(a => a.isIssued === false);
+    if (availableBooks.length === 0) return res.status(404).json({ message: "No available books found" });
+    return res.status(200).json(availableBooks);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
 module.exports = {
@@ -52,8 +82,8 @@ module.exports = {
   getBooksByGenre,
   getBooksByCupboard,
   getBooksByShelf,
-  getBooksByCupboardAndShelf,
   getBookById,
+  getBooksByCupboardAndShelf,
   getIssuedBooks,
   getAvailableBooks,
 };
